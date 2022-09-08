7256518c1bb0fb0b83e27825aa298700d34560bc
robbyhdayatt <112568253+robbyhdayatt@users.noreply.github.com>
2022-09-08 09:44:46 +0700

    Add files via upload

---
+++ praktikum 2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int s, k;
+    cout << "masukkan panjang sisi persegi: ";
+    cin >> s;
+    k = 4*s;
+    cout << "keliling persegi adalah "<< k << endl;
+    return 0;
+}
